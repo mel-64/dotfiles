d77fa6fc0899ec3b601c44bb98864adb8298e52e
melody <admin@melthecat.dev>
2026-06-04 11:34:27 +0200

    add minimal user.js

---
+++ .config/librewolf/librewolf/default-release/user.js
@@ -0,0 +1,18 @@
+// Disable JIT
+user_pref("javascript.options.baselinejit", false);
+user_pref("javascript.options.ion", false);
+user_pref("javascript.options.asmjs", false);
+
+// Set Custom Font
+user_pref("font.name.serif.x-western", "0xProto Nerd Font");
+
+// Disable middlemouse paste
+user_pref("middlemouse.paste", false);
+
+// Privacy
+user_pref("privacy.fingerprintingProtection", true);
+user_pref("privacy.fingerprintingProtection.overrides", "+AllTargets,-CSSPrefersColorScheme,-JSDateTimeUTC");
+user_pref("privacy.clearOnShutdown_v2.cache", true);
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", true);
+user_pref("privacy.clearSiteData.cookiesAndStorage", true);
+
