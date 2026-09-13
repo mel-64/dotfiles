6ed381db5441e0a3673101c3d11d7517c5d2cdef
melody <admin@melthecat.dev>
2026-09-13 19:46:23 +0200

    ugh, way too much shit breaks if sourced from external file so back to single file

--- .config/zsh/empty.sh
+++
@@ -1,1 +0,0 @@
-# Empty file so that `$HOME/.config/zsh/*.sh` always matches at least one file.
--- .config/zsh/opts.zsh
+++
@@ -1,6 +0,0 @@
-setopt inc_append_history
-setopt hist_ignore_space # Don't save to history if space in front
-setopt autocd # Allow cd-ing into dir by executing it
-setopt interactive_comments # Comments in normal shell
-setopt hist_expire_dups_first
-
