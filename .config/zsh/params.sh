a894b5665c0f445ba936411c37c3a43e7baaec35
melody <admin@melthecat.dev>
2026-09-13 14:23:07 +0200

    export zsh config params in params.sh

--- .config/zsh/params.sh
+++ .config/zsh/params.sh
@@ -1,5 +1,6 @@
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+export HISTFILE=~/.zsh_history
+export HISTSIZE=10000
+export SAVEHIST=10000
 
-fpath+=~/.zfunc # Autocompletion
+export fpath+=~/.zfunc # Autocompletion
+
